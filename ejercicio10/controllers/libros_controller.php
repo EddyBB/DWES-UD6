1fc7ac8677f05d569fc445d13c53f3961a1fdf9d
Eddy <eddy.gonzalez.chavez.al@iespoligonosur.org>
2022-01-21 14:31:07 +0100

    terminar mostrar 1 libro

---
+++ ejercicio10/controllers/libros_controller.php
@@ -0,0 +1,23 @@
+<?php
+
+    function listar(){
+        // se incluye el modelo
+        require './models/libros_model.php';
+
+        //En $libros tenemos un array con todos los libros gracias al modelo
+        $libros = getLibros();
+
+        //en $libros tememos un array
+        include './views/libros_view.php';
+    }
+
+    function unicoLibro(){
+        require './models/libros_model.php';
+
+        $id = $_GET['id'];
+
+        $unicoLibro = getLibro($id);
+        
+        include './views/libros_view.php';
+    }
+?>
---
+++ ejercicio10/models/libros_model.php
@@ -0,0 +1,28 @@
+<?php
+
+    function getConnection(){
+        $user = 'root';
+        $password = 'root';
+        return new PDO('mysql:host=localhost;dbname=biblioteca', $user, $password);
+    }
+        
+    function getLibros(){
+        $db = getConnection();
+        $result = $db->query('SELECT titulo, precio FROM libros');
+        $libros = array();
+        while ($libro = $result->fetch()) {
+            $libros[] = $libro;
+        }
+
+        return $libros;
+    }
+
+    function getLibro($id){
+        $db = getConnection();
+        $result = $db->prepare('SELECT titulo, precio FROM libros WHERE id=:id');
+        $result->bindParam(":id",$id);
+        $result->execute();
+
+        return $result->fetch();
+    }
+?>
